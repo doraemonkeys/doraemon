b012e620ac2dbaf245c04cf1bd9c0c11aa8f0eb4
doraemon <doraemonkey@qq.com>
2022-11-25 00:44:51 +0800

    update

--- exec.go
+++ exec.go
@@ -6,8 +6,9 @@ import (
 	"time"
 )
 
-//不等待执行完毕就返回(可能不靠谱),如果params中有转义字符需要自己处理,
-//dir为cmd命令执行的位置,传入空值则为默认路径。
+// 不等待执行完毕就返回(go程序不能立即退出),如果params中有转义字符需要自己处理,
+// dir为cmd命令执行的位置,传入空值则为默认路径。
+// params中的命令会用空格分隔，一次性提交给cmd。
 func Cmd_NoWait(dir string, params []string) (cmd *exec.Cmd, err error) {
 	cmd = exec.Command("cmd")
 	cmd_in := bytes.NewBuffer(nil)
@@ -34,8 +35,9 @@ func Cmd_NoWait(dir string, params []string) (cmd *exec.Cmd, err error) {
 	return cmd, nil
 }
 
-//等待执行完毕才返回,
-//dir为cmd命令执行的位置,传入空值则为默认路径
+// 等待执行完毕才返回,
+// dir为cmd命令执行的位置,传入空值则为默认路径。
+// params中的命令会用空格分隔，一次性提交给cmd。
 func Cmd(dir string, params []string) (string, error) {
 	cmd := exec.Command("cmd")
 	cmd_in := bytes.NewBuffer(nil)
@@ -61,7 +63,9 @@ func Cmd(dir string, params []string) (string, error) {
 	return string(GbkToUtf8(output)), nil
 }
 
-//等待执行完毕才返回,不反回输出
+// 等待执行完毕才返回,不反回输出。
+// dir为cmd命令执行的位置,传入空值则为默认路径。
+// params中的命令会用空格分隔，一次性提交给cmd。
 func CmdNoOutput(dir string, params []string) error {
 	cmd := exec.Command("cmd")
 	cmd_in := bytes.NewBuffer(nil)
