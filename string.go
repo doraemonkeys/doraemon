b4dd574550704e764c5f1e605cf418f5d8d851d4
doraemon <doraemonkey@qq.com>
2023-04-12 16:36:22 +0800

    update

--- string.go
+++ string.go
@@ -1,1 +1,14 @@
 package doraemon
+
+import "unsafe"
+
+func StringToReadOnlyBytes(s string) []byte {
+	return unsafe.Slice(unsafe.StringData(s), len(s))
+}
+
+// BytesToString converts a byte slice to a string without copying.
+// The byte slice must not be modified after the conversion.
+// otherwise, the string may be corrupted.
+func BytesToString(b []byte) string {
+	return unsafe.String(&b[0], len(b))
+}
