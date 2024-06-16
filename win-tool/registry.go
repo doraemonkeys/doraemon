68c5461c62f1dda99a2e43d2fbe3914344c7f11a
doraemon <doraemonkey@qq.com>
2024-06-16 22:39:38 +0800

    feat: Add functions to query user and system environment paths

--- win-tool/registry.go
+++ win-tool/registry.go
@@ -1,3 +1,55 @@
 //go:build windows
 
 package win_tool
+
+import (
+	"errors"
+	"os/exec"
+	"strings"
+)
+
+func QueryUserEnvironmentPath() ([]string, error) {
+	pathCmd := exec.Command("reg", "query", "HKEY_CURRENT_USER\\Environment", "/v", "Path")
+	pathByte, err := pathCmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	nowPath := strings.TrimSpace(string(pathByte))
+	keyWords := "Path    REG_SZ"
+	if !strings.Contains(nowPath, keyWords) {
+		return nil, errors.New("query path failed")
+	}
+	nowPath = strings.TrimSpace(nowPath[strings.LastIndex(nowPath, keyWords)+len(keyWords):])
+	nowPathSlice := strings.Split(nowPath, ";")
+	for i := 0; i < len(nowPathSlice); i++ {
+		nowPathSlice[i] = strings.TrimSpace(nowPathSlice[i])
+		if nowPathSlice[i] == "" {
+			nowPathSlice = append(nowPathSlice[:i], nowPathSlice[i+1:]...)
+			i--
+		}
+	}
+	return nowPathSlice, nil
+}
+
+func QuerySystemEnvironmentPath() ([]string, error) {
+	pathCmd := exec.Command("reg", "query", "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session Manager\\Environment", "/v", "Path")
+	pathByte, err := pathCmd.Output()
+	if err != nil {
+		return nil, err
+	}
+	nowPath := strings.TrimSpace(string(pathByte))
+	keyWords := "Path    REG_SZ"
+	if !strings.Contains(nowPath, keyWords) {
+		return nil, errors.New("query path failed")
+	}
+	nowPath = strings.TrimSpace(nowPath[strings.LastIndex(nowPath, keyWords)+len(keyWords):])
+	nowPathSlice := strings.Split(nowPath, ";")
+	for i := 0; i < len(nowPathSlice); i++ {
+		nowPathSlice[i] = strings.TrimSpace(nowPathSlice[i])
+		if nowPathSlice[i] == "" {
+			nowPathSlice = append(nowPathSlice[:i], nowPathSlice[i+1:]...)
+			i--
+		}
+	}
+	return nowPathSlice, nil
+}
